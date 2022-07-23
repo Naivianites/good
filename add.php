eb94c83c065d714b4979603e53cad0056bdd9597
Naivians <109004810+Naivians@users.noreply.github.com>
2022-07-23 09:19:33 +0800

    first commit

---
+++ add.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/add.css">
+    <title>Edit Qoutes</title>
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <div class="banner">
+                <h1 class="title">The Good Qoutes</h1>
+                <a href="index.php">Home</a>
+            </div>
+        </header>
+        
+        <!-- add qoutes sections -->
+        <section class="add-container">
+            <div class="add-container-top">
+                <div class="add">
+                    <h2>Add Qoutes</h2>
+                </div>
+            </div>
+           <div class="add-form">
+                <form action="#" method="post">
+                    <div class="inputs">
+                        <p>Qoute</p>
+                        <!-- <input type="text" name="qoute" placeholder="Be the Best version of yourself"> -->
+                        <textarea name="qoute">
+
+                        </textarea>
+                    </div>
+                    <div class="inputs">
+                        <p>Author </p>
+                        <input type="text" name="qoute" placeholder="(If you want your name to be anonymous its completely OPTIONAL)">
+                    </div>
+                    <div class="btn">
+                        <a href="#" id="submit">Submit</a>
+                        <a href="index.php" id="close">Close</a>
+                    </div>
+                </form>
+           </div>
+        </section>
+    </div>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>The Good Qoutes</title>
+</head>
+
+<body>
+    <div class="wrapper">
+        <header>
+            <div class="banner">
+                <h1 class="title">The Good Quotes</h1>
+                <a href="index.php">Home</a>
+            </div>
+        </header>
+        
+        <!-- descriptions -->
+        <section class="description">
+            <div class="des-container">
+                <h2>Share Your Motivational and <br> Inspirational Qoutes</h2>
+                <a href="add.php">Add Qoutes</a>
+            </div>
+        </section>
+        <div class="search">
+            <input type="text" name="seach" placeholder="Search By Author name">
+        </div>
+        <!-- display qoutes -->
+        <section class="quote-container">
+            <a href="edit.php" class="quotes" >
+                <div class="quote">
+                    <h2>" The fear of the Lord is the beginning of knowledge but fools despise wisdom and instuctions " - Proverbs 1:7</h2>
+                </div>
+                <div class="publisher">
+                    <p>Naivians</p>
+                </div>
+            </a>
+            <a href="edit.php" class="quotes" >
+                <div class="quote">
+                    <h2>" The fear of the Lord is the beginning of knowledge but fools despise wisdom and instuctions " - Proverbs 1:7</h2>
+                </div>
+                <div class="publisher">
+                    <p>Naivians</p>
+                </div>
+            </a>
+            <a href="edit.php" class="quotes" >
+                <div class="quote">
+                    <h2>" The fear of the Lord is the beginning of knowledge but fools despise wisdom and instuctions " - Proverbs 1:7</h2>
+                </div>
+                <div class="publisher">
+                    <p>Naivians</p>
+                </div>
+            </a>
+            <a href="edit.php" class="quotes" >
+                <div class="quote">
+                    <h2>" The fear of the Lord is the beginning of knowledge but fools despise wisdom and instuctions " - Proverbs 1:7</h2>
+                </div>
+                <div class="publisher">
+                    <p>Naivians</p>
+                </div>
+            </a>
+        </section>
+    </div>
+
+
+
+
+</body>
+
+</html>
